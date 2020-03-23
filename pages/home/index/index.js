7499f416070e6223cfe097177f5c0ea359d675d6
Huage <2334788801@qq.com>
2020-03-23 18:10:21 +0800

    完成tabbar

--- pages/home/index/index.js
+++ pages/home/index/index.js
@@ -17,14 +17,35 @@ Page({
       { name: '我的', icon: 'icon-wodedangxuan', state: 4 },
     ],
     tabcurren: 0,
-    tabbar: false,
+    state: 1,
+    tabbar: false,    //没有嘉宾为true/有为false
   },
 
   tabbar(e) {
     // console.log(e)
-    this.setData({
-      tabcurren: e.currentTarget.dataset.curren
-    })
+    // var that = this
+    var index = e.currentTarget.dataset.curren
+    
+    // console.log(state.state)
+    if(this.data.tabbar){
+      // 循环没有嘉宾的tabbar
+      var state1 = this.data.tabbar1[index].state
+      console.log(1)
+      this.setData({
+        tabcurren: e.currentTarget.dataset.curren,
+        state: state1,
+      })
+    }else {
+      // 循环有嘉宾的tabbar
+      var state = this.data.tabbar2[index].state
+      console.log(2)
+      this.setData({
+        tabcurren: e.currentTarget.dataset.curren,
+        state: state,
+      })
+    }
+    
+    // state.state
   },
   /**
    * 生命周期函数--监听页面加载
